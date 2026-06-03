575f7acf5722263e9405ca4cf60c5a369fb36afb
FelipeNeves126 <felipeneves126@gmail.com>
2026-06-03 13:44:18 -0300

    nome por enquanto -UPLIft-

--- script/script.js
+++ script/script.js
@@ -1,14 +1,13 @@
-function abrirMenu(){
-    const btnMenu = document.getElementById('menu-btn');
-    const menuMobile = document.getElementById('menu-mobile');
 
-    btnMenu.addEventListener('click', () => {
-        menuMobile.classList.toggle('aberto');
+const btnMenu = document.getElementById('menu-btn');
+const menuMobile = document.getElementById('menu-mobile');
 
-        if (menuMobile.classList.contains('aberto')) {
-                btnMenu.innerText = '✕';
-        } else {
-                btnMenu.innerText = '☰';
-        }
-    });
+btnMenu.addEventListener('click', () => {
+menuMobile.classList.toggle('aberto');
+
+if (menuMobile.classList.contains('aberto')) {
+    btnMenu.innerText = '✕';
+} else {
+    btnMenu.innerText = '☰';
 }
+});
