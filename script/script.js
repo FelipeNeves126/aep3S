f33645a5bb5794216e3062a80db813e6c2d67f9f
FelipeNeves126 <felipeneves126@gmail.com>
2026-06-02 22:18:31 -0300

    configuração menu -mobile-

---
+++ script/script.js
@@ -0,0 +1,14 @@
+function abrirMenu(){
+    const btnMenu = document.getElementById('menu-btn');
+    const menuMobile = document.getElementById('menu-mobile');
+
+    btnMenu.addEventListener('click', () => {
+        menuMobile.classList.toggle('aberto');
+
+        if (menuMobile.classList.contains('aberto')) {
+                btnMenu.innerText = '✕';
+        } else {
+                btnMenu.innerText = '☰';
+        }
+    });
+}
