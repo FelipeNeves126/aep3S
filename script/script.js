a26de97e02b1417d6997a8d6fe342746f72a2e38
FelipeNeves126 <felipeneves126@gmail.com>
2026-06-03 21:33:53 -0300

    ajuste perfil ususario

--- script/script.js
+++ script/script.js
@@ -11,3 +11,21 @@ if (menuMobile.classList.contains('aberto')) {
     btnMenu.innerText = '☰';
 }
 });
+
+const btnAvatar = document.getElementById('btn-avatar');
+const modalPerfil = document.getElementById('modal-perfil');
+const btnFecharModal = document.getElementById('fechar-modal');
+
+btnAvatar.addEventListener('click', () =>{
+    modalPerfil.classList.add('ativo');
+});
+
+btnFecharModal.addEventListener('click', () => {
+    modalPerfil.classList.remove('ativo');
+});
+
+modalPerfil.addEventListener('click', (evento) => {
+    if(evento.target === modalPerfil){
+        modalPerfil.classList.remove('ativo');
+    }
+});
