4755e172700c87a79de0f7f8fa74ca9479ec68b7
FelipeNeves126 <felipeneves126@gmail.com>
2026-06-05 23:00:42 -0300

    organização modais abertura e fechamentoi

--- script/script.js
+++ script/script.js
@@ -14,41 +14,19 @@ if (menuMobile.classList.contains('aberto')) {
 
 const btnAvatar = document.getElementById('btn-avatar');
 const modalPerfil = document.getElementById('modal-perfil');
-const btnFecharModal = document.getElementById('fechar-modal');
 
 btnAvatar.addEventListener('click', () =>{
     modalPerfil.classList.add('ativo');
 });
 
-btnFecharModal.addEventListener('click', () => {
-    modalPerfil.classList.remove('ativo');
-});
-
-modalPerfil.addEventListener('click', (evento) => {
-    if(evento.target === modalPerfil){
-        modalPerfil.classList.remove('ativo');
-    }
-});
-
 const btnIniciarCadastro = document.getElementById('btn-iniciar-cadastro');
 const modalTipoCadastro = document.getElementById('modal-tipo-cadastro');
-const btnFecharTipoCadastro = document.getElementById('fechar-tipo-cadastro');
 
 if (btnIniciarCadastro) {
     btnIniciarCadastro.addEventListener('click', () => {
         modalTipoCadastro.classList.add('ativo');
     });
 }
-
-btnFecharTipoCadastro.addEventListener('click', () => {
-    modalTipoCadastro.classList.remove('ativo');
-});
-
-modalTipoCadastro.addEventListener('click', (evento) => {
-    if (evento.target === modalTipoCadastro){
-        modalTipoCadastro.classList.remove('ativo');
-    }
-});
 
 const btnEscolhaDoador = document.getElementById('btn-escolha-doador');
 const btnEscolhaEmpresa = document.getElementById('btn-escolha-empresa');
@@ -59,4 +37,41 @@ btnEscolhaDoador.addEventListener('click', () => {
 
 btnEscolhaEmpresa.addEventListener('click', () => {
     modalTipoCadastro.classList.remove('ativo');
-})
+});
+
+const linkMenuCadastro = document.getElementById('link-menu-cadastro');
+
+if (linkMenuCadastro) {
+    linkMenuCadastro.addEventListener('click', (evento) => {
+        evento.preventDefault();
+
+        const menuMobile = document.getElementById('menu-mobile');
+        const btnMenu = document.getElementById('menu-btn');
+        menuMobile.classList.remove('aberto');
+        btnMenu.innerHTML = `☰`;
+
+        modalTipoCadastro.classList.add('ativo');
+    });
+};
+
+const todosBotoesFechar = document.querySelectorAll('.js-fechar-modal');
+
+todosBotoesFechar.forEach(botao => {
+    botao.addEventListener('click', function() {
+
+        const modalPai = this.closest('.js-modal-overlay');
+        if(modalPai){
+            modalPai.classList.remove('ativo');
+        }
+    });
+});
+
+const todosModais = document.querySelectorAll('.js-modal-overlay');
+
+todosModais.forEach(modal => {
+    modal.addEventListener('click', function(evento) {
+        if (evento.target === this) {
+            this.classList.remove('ativo');
+        }
+    });
+});
